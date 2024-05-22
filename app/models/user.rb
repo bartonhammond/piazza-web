f1edc62d7161152dd797f8e5d88382c8b09e0e9a
Barton Hammond <bartonhammond@gmail.com>
2024-05-22 18:41:42 -0500

    Refactor auth tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,27 +1,20 @@
 class User < ApplicationRecord
+  include Authentication
+
   validates :name, presence: true
   validates :email,
             format: { with: URI::MailTo::EMAIL_REGEXP },
             uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: { minimum: 8 }
 
   has_many :memberships, dependent: :destroy
   has_many :organizations, through: :memberships
-  has_many :app_sessions
 
-  normalizes :name, with: ->(name) { name.strip }
-  normalizes :email, with: ->(email) { email.strip.downcase }
+  before_validation :strip_extraneous_spaces
 
-  has_secure_password
+  private
 
-  def self.create_app_session(email:, password:)
-    user = User.authenticate_by(email:, password:)
-    user.app_sessions.create if user.present?
-  end
-
-  def authenticate_app_session(app_session_id, token)
-    app_sessions.find(app_session_id).authenticate_token(token)
-  rescue ActiveRecord::RecordNotFound
-    nil
+  def strip_extraneous_spaces
+    self.name = name&.strip
+    self.email = email&.strip
   end
 end
