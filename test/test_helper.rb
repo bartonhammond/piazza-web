aaafa3c27dee594b71fb51cd6a7832b32fbb1810
Barton Hammond <bartonhammond@gmail.com>
2024-05-22 18:19:55 -0500

    Test concern

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,10 @@
-ENV["RAILS_ENV"] ||= "test"
-require_relative "../config/environment"
-require "rails/test_help"
+ENV['RAILS_ENV'] ||= 'test'
+require_relative '../config/environment'
+require 'rails/test_help'
 
+Dir[Rails.root.join('test', 'support', '**', '*.rb')].each do |f|
+  require f
+end
 module ActiveSupport
   class TestCase
     # Run tests in parallel with specified workers
@@ -12,4 +15,9 @@ module ActiveSupport
 
     # Add more helper methods to be used by all tests here...
   end
+end
+
+class ActionDispatch::IntegrationTest
+  include AuthenticationHelpers
+  include RoutesHelpers
 end
